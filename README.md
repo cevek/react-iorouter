398e31772f2c4100d3f51293b1813b35ca0eb6c6
cody <cody@MacBook-Pro-Arhur.local>
2020-03-21 17:39:22 +0300

    Init

---
+++ README.md
@@ -0,0 +1,87 @@
+# react-iorouter
+[![MinGzip](https://badgen.net/bundlephobia/minzip/react-iorouter)](https://bundlephobia.com/result?p=react-iorouter)
+
+Simple small react router with typed routes and SSR support
+
+## Features
+
+🔥 100% Typed
+
+🚀 React hooks
+
+⚡️️ 2kb gzipped
+
+🦄 Node SSR Rendering
+
+💎 Scroll restoration
+
+🎹 Optional query parameters
+
+## Example
+
+```tsx
+import * as React from 'react';
+import * as ReactDOM from 'react-dom';
+import {createRoute, Router, Route, Link, useRedirect} from 'react-iorouter';
+
+export const routes = {
+    index: createRoute<{tab?: string}>('/'),
+    todos: createRoute('/todos'),
+    editTodo: createRoute<{id: string}>('/todo/edit/:id'),
+};
+
+function App() {
+    return (
+        <Router>
+            <div>
+                <Link route={routes.index} params={{}}>
+                    go to index
+                </Link>
+            </div>
+            <div>
+                <Link route={routes.todos} params={{}}>
+                    go to todos
+                </Link>
+            </div>
+            <Route route={routes.todos}>
+                <Todos />
+            </Route>
+            <Route route={routes.editTodo}>{params => <EditTodo id={params.id} />}</Route>
+        </Router>
+    );
+}
+function Todos() {
+    const todos = [
+        {id: '1', name: 'foo'},
+        {id: '2', name: 'bar'},
+    ];
+    return (
+        <div>
+            {todos.map(todo => (
+                <div key={todo.id}>
+                    {todo.name}{' '}
+                    <Link route={routes.editTodo} params={{id: todo.id}}>
+                        Edit
+                    </Link>
+                </div>
+            ))}
+        </div>
+    );
+}
+
+function EditTodo(props: {id: string}) {
+    const redirect = useRedirect();
+    const save = async () => {
+        // await api.saveTodo({name});
+        redirect(routes.todos, {});
+    };
+    return (
+        <div>
+            <input />
+            <button onClick={save}>Save</button>
+        </div>
+    );
+}
+
+ReactDOM.render(<App />, document.getElementById('root'));
+```
---
+++ index.tsx
@@ -0,0 +1,189 @@
+import {Key, parse, regexpToFunction, tokensToFunction, tokensToRegexp} from 'path-to-regexp';
+import * as React from 'react';
+
+export const routes = {
+    profile: createRoute('/'),
+    attendanceManagers: createRoute('/attendance-managers'),
+    editAttendanceManagers: createRoute<{id: string; foo?: string}>('/attendance-managers/edit/:id'),
+    addAttendanceManagers: createRoute('/attendance-managers/add'),
+};
+
+type Route<Params extends object> = {
+    path: string;
+    matchUrl: (url: Url) => Params | null;
+    testUrl: (path: string) => boolean;
+    toUrl: (params: Params) => string;
+};
+
+function createRoute<Params extends object>(path: string): Route<Params> {
+    const keys: Key[] = [];
+    const tokens = parse(path);
+    const re = tokensToRegexp(tokens, keys);
+    const _match = regexpToFunction<Params>(re, keys, {decode: decodeURIComponent});
+    const _toUrl = tokensToFunction<Params>(tokens, {encode: encodeURIComponent});
+    const requiredFields = new Set(
+        tokens
+            .map(token => (typeof token !== 'string' ? String(token.name) : undefined))
+            .filter((t): t is string => t !== undefined),
+    );
+    const matchUrl = (url: Url) => {
+        const m = _match(url.path);
+        if (m === false) {
+            return null;
+        }
+        const params = m.params;
+        url.query.split('&').forEach(p => {
+            const r = p.split('=');
+            const key = decodeURIComponent(r[0]) as never;
+            const val = decodeURIComponent(r[1]) as never;
+            if (!(key in params)) {
+                params[key] = val;
+            }
+        });
+        return params;
+    };
+    const testUrl = (path: string) => {
+        return _match(path) !== false;
+    };
+    const toUrl = (params: Params) => {
+        let query = '';
+        for (const key in params) {
+            if (!requiredFields.has(key)) {
+                query += `${encodeURIComponent(key)}=${encodeURIComponent((params[key] as unknown) as string)}`;
+            }
+        }
+        return _toUrl(params) + (query === '' ? '' : `?${query}`);
+    };
+    return {
+        path: path,
+        matchUrl: matchUrl,
+        testUrl: testUrl,
+        toUrl: toUrl,
+    };
+}
+
+type Router = {
+    url: Url;
+    push: (url: string) => void;
+    replace: (url: string) => void;
+};
+
+type Url = {
+    path: string;
+    query: string;
+    state: object | undefined;
+};
+const createUrl = (): Url => ({
+    path: window.location.pathname,
+    query: window.location.search.substr(1),
+    state: window.history.state,
+});
+export const Router = React.memo(function Router(props: {
+    initialUrl?: string;
+    children: React.ReactNode;
+    scrollRestoration?: boolean;
+    onBeforeNextUrl?: (url: string) => object;
+    onAfterNextUrl?: (url: string) => object;
+    onBackForward?: (url: string) => object;
+}) {
+    const {children, initialUrl, onBeforeNextUrl, onAfterNextUrl, onBackForward, scrollRestoration = true} = props;
+    const defaultUrl = (): Url => {
+        if (initialUrl === undefined) {
+            return createUrl();
+        }
+        const p = initialUrl.split('?');
+        return {
+            path: p[0],
+            query: p[1] || '',
+            state: undefined,
+        };
+    };
+    const [url, setUrl] = React.useState(defaultUrl);
+    const [fromBackForward, setBackForwardState] = React.useState(false);
+    const change = (type: 'pushState' | 'replaceState') => (url: string) => {
+        if (typeof window === 'object') {
+            const state = onBeforeNextUrl && onBeforeNextUrl(url);
+            window.history[type](state, '', url);
+            onAfterNextUrl && onAfterNextUrl(url);
+            setUrl(createUrl());
+            setBackForwardState(false);
+        }
+    };
+    const router: Router = {
+        push: change('pushState'),
+        replace: change('replaceState'),
+        url: url,
+    };
+    React.useEffect(() => {
+        if (scrollRestoration) {
+            window.history.scrollRestoration = 'auto';
+        }
+        const fn = () => {
+            onBackForward && onBackForward(window.location.href);
+            setUrl(createUrl());
+            setBackForwardState(true);
+        };
+        window.addEventListener('popstate', fn);
+        return () => window.removeEventListener('popstate', fn);
+    }, []);
+
+    React.useLayoutEffect(() => {
+        if (!fromBackForward && scrollRestoration) {
+            window.scrollTo(0, 0);
+        }
+    }, [fromBackForward, scrollRestoration]);
+    return <routerContext.Provider value={router}>{children}</routerContext.Provider>;
+});
+
+const routerContext = React.createContext<Router>(null!);
+
+export function Route<Params extends object>(props: {
+    route: Route<Params>;
+    children: React.ReactNode | ((params: Params) => React.ReactNode);
+}) {
+    const {children, route} = props;
+    const router = React.useContext(routerContext);
+    if (typeof children === 'function') {
+        const res = route.matchUrl(router.url);
+        if (res !== null) {
+            return <>{children(res)}</>;
+        }
+        return null;
+    }
+    if (route.testUrl(router.url.path)) {
+        return <>{children}</>;
+    }
+    return null;
+}
+
+export function Link<Params extends object>(props: {
+    route: Route<Params>;
+    params: Params;
+    className?: string;
+    children: React.ReactNode;
+}) {
+    const {children, params, route} = props;
+    const router = React.useContext(routerContext);
+    const url = route.toUrl(params);
+    return (
+        <a
+            href={url}
+            className={props.className}
+            onClick={e => {
+                if (!e.ctrlKey && !e.metaKey) {
+                    e.preventDefault();
+                    return router.push(url);
+                }
+            }}>
+            {children}
+        </a>
+    );
+}
+
+export function useRedirect() {
+    const router = React.useContext(routerContext);
+    return <Params extends object>(route: Route<Params>, params: Params) => {
+        const url = route.toUrl(params);
+        router.push(url);
+    };
+}
